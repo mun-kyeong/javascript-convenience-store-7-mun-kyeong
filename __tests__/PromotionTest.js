95a3ac45e52b33e2f88bf59dec55f0cf7f2ae0b2
mun-kyeong <horse6953@naver.com>
2024-11-10 02:16:25 +0900

    [FEAT] (promotion.js) : promotion 행사 정보 저장
    - promotion 행사 정보 저장
    - products와 마찬가지로 { key:value } 형태로 저장
    	- 인덱싱을 조금 더 빠르고 효율적이게 하기 위함
    
    읽어온 promotion 행사 정보를 저장하고 값을 return 합니다.

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -1,3 +1,6 @@
+import { Promotion } from "../src/Promotion";
+import { readDocs } from "../src/utils/readDocs";
+
 describe("프로모션 행사 test ", () => {
   let promotion;
   beforeEach(async () => {
@@ -5,9 +8,18 @@ describe("프로모션 행사 test ", () => {
     promotion = new Promotion(promotions);
   });
   test.each([
-    ["탄산2+1", [2, 1, "2024-01-01", "2024-12-31"]],
-    ["MD추천상품", [1, 1, "2024-01-01", "2024-12-31"]],
-    ["반짝할인", [1, 1, "2024-11-01", "2024-11-30"]],
+    [
+      "탄산2+1",
+      { buy: 2, get: 1, start_date: "2024-01-01", end_date: "2024-12-31" },
+    ],
+    [
+      "MD추천상품",
+      { buy: 1, get: 1, start_date: "2024-01-01", end_date: "2024-12-31" },
+    ],
+    [
+      "반짝할인",
+      { buy: 1, get: 1, start_date: "2024-11-01", end_date: "2024-11-30" },
+    ],
   ])("프로모션 행사 정보를 저장한다.", (promotionName, promotionValue) => {
     expect(promotion.getPromotionInfo(promotionName)).toEqual(promotionValue);
   });
--- src/Promotion.js
+++ src/Promotion.js
@@ -1,1 +1,21 @@
-export class Promotion {}
+export class Promotion {
+  #promotions;
+
+  constructor(promotions) {
+    this.#promotions = {};
+    promotions.forEach((promotion) => this.#savePromotions(promotion));
+  }
+
+  #savePromotions(promotion) {
+    return (this.#promotions[promotion[0]] = {
+      buy: Number(promotion[1]),
+      get: Number(promotion[2]),
+      start_date: promotion[3],
+      end_date: promotion[4],
+    });
+  }
+
+  getPromotionInfo(promotionName) {
+    return this.#promotions[promotionName];
+  }
+}
