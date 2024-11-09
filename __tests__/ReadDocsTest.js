12109ef12b6b2e18a4ed003c98c9e4983e0aaa8a
mun-kyeong <horse6953@naver.com>
2024-11-10 02:02:21 +0900

    [TEST] (ReadDocsTest.js) : product.md 파일 읽어오기
    - product.md 파일 읽어오기
    
    product.md 파일을 읽어옵니다. - test 수정

--- __tests__/ReadDocsTest.js
+++ __tests__/ReadDocsTest.js
@@ -1,6 +1,6 @@
 import { readDocs, readFileInfo, readProducts } from "../src/utils/readDocs.js";
 
-describe("Docs파일 읽어오기", () => {
+describe("products.md 파일 읽어오기", () => {
   let products;
   beforeEach(async () => {
     products = await readDocs("products");
@@ -16,5 +16,30 @@ describe("Docs파일 읽어오기", () => {
     [5, ["오렌지주스", "1800", "9", "MD추천상품"]],
   ])("products.md 파일에서 %d번째 라인 읽어오기", (index, expected) => {
     expect(products[index]).toEqual(expected);
+  });
+});
+
+describe("promotions.md 파일 읽어오기", () => {
+  let promotions;
+  beforeEach(async () => {
+    promotions = await readDocs("promotions");
+  });
+
+  test("promotions.md 파일에서 첫 번째 라인 읽어오기", () => {
+    expect(promotions[0]).toEqual([
+      "name",
+      "buy",
+      "get",
+      "start_date",
+      "end_date",
+    ]);
+  });
+
+  test.each([
+    [1, ["탄산2+1", "2", "1", "2024-01-01", "2024-12-31"]],
+    [2, ["MD추천상품", "1", "1", "2024-01-01", "2024-12-31"]],
+    [3, ["반짝할인", "1", "1", "2024-11-01", "2024-11-30"]],
+  ])("products.md 파일에서 %d번째 라인 읽어오기", (index, expected) => {
+    expect(promotions[index]).toEqual(expected);
   });
 });
