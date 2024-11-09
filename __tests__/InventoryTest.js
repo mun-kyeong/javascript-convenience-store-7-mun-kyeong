e9b1d893b999fc9f50eb8b2b946d2221a67a9c78
mun-kyeong <horse6953@naver.com>
2024-11-09 22:10:34 +0900

    [TEST] (InventoryTest) : 인벤토리 클래스 인스턴스 확인 test
    - products.md 파일로 읽어온 값이 인벤토리에 인스턴스 변수로 저장이 되는지 확인
    
    products.md 파일로 읽어온 값이 인벤토리에 인스턴스 변수로 저장이 되는지 test 합니다

--- __tests__/InventoryTest.js
+++ __tests__/InventoryTest.js
@@ -6,9 +6,19 @@ describe("재고 관리 기능 테스트", () => {
     products = await readProducts();
   });
 
-  test("각 상품의 재고 수량을 고려하여 결제 가능 여부를 확인한다.", () => {
-    const inventory = new Inventory(products);
-    const USER_ORDER = { 콜라: 10, 사이다: 3 };
-    expect(inventory.checkOrder(USER_ORDER)).toBe(true);
-  });
+  test.each([
+    [1, { name: "콜라", price: 1000, quantity: 10, promotion: "탄산2+1" }],
+  ])(
+    "Inventory 클래스의 %d 번째 상품이 저장되는지 확인한다.",
+    (index, result) => {
+      const inventory = new Inventory(products);
+      expext(inventory.getProductInfo(index)).toEqual(result);
+    }
+  );
+
+  //   test("각 상품의 재고 수량을 고려하여 결제 가능 여부를 확인한다.", () => {
+  //     const inventory = new Inventory(products);
+  //     const USER_ORDER = { 콜라: 10, 사이다: 3 };
+  //     expect(inventory.checkOrder(USER_ORDER)).toBe(true);
+  //   });
 });
