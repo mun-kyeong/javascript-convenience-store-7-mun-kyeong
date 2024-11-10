c969c113062752ccbd038b0193c71f1ce7e549c4
mun-kyeong <horse6953@naver.com>
2024-11-10 13:37:42 +0900

    [FEAT] (Receipt.js) : 영수증 order 가격 정보 추가
    - 사용자가 주문한 목록에 가격 정보를 추가해서 저장
    
    사용자가 주문한 목록을 저장하고 가져옵니다.

--- __tests__/ReceiptTest.js
+++ __tests__/ReceiptTest.js
@@ -1,12 +1,30 @@
+import { Console } from "@woowacourse/mission-utils";
 import { Receipt } from "../src/Receipt";
+import { Inventory } from "../src/repository/Inventory";
+import { readDocs } from "../src/utils/readDocs";
 
 describe("영수증(Receipt) 클래스 테스트", () => {
+  let inventory;
+  beforeEach(async () => {
+    const products = await readDocs("products");
+    inventory = new Inventory(products);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
   test("물품 구매 시 영수증에 구매한 상품명, 수량, 가격 저장", () => {
     const USER_ORDER = [
       ["콜라", 2],
       ["사이다", 1],
     ];
-    const receipt = new Receipt(USER_ORDER);
-    expect(receipt.getUserOrder()).toEqual(USER_ORDER);
+    const USER_ORDER_INFO = {
+      콜라: { quantity: 2, price: 1000 },
+      사이다: { quantity: 1, price: 1000 },
+    };
+    const receipt = new Receipt(USER_ORDER, inventory);
+    expect(receipt.getUserOrder()).toEqual(USER_ORDER_INFO);
   });
 });
--- src/Receipt.js
+++ src/Receipt.js
@@ -1,11 +1,25 @@
-export class Receipt {
-  #order;
+import { Console } from "@woowacourse/mission-utils";
+import { Inventory } from "./repository/Inventory";
 
-  constructor(order) {
-    this.#order = order;
+export class Receipt {
+  #userOrder = {};
+  #inventory;
+
+  constructor(orders, inventory) {
+    this.#inventory = inventory;
+    orders.forEach((order) => {
+      this.#userOrder[order[0]] = this.#addOrderPirce(order);
+    });
+  }
+
+  #addOrderPirce(order) {
+    return {
+      price: this.#inventory.getProductInfo(order[0]).price,
+      quantity: order[1],
+    };
   }
 
   getUserOrder() {
-    return this.#order;
+    return this.#userOrder;
   }
 }
